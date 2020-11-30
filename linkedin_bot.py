3a9cfe47934ec843732ba55f2cb31295734d13dc
GabrielPastorello <57769272+GabrielPastorello@users.noreply.github.com>
2020-11-30 14:40:57 -0300

    Update linkedin_bot.py

--- linkedin_bot.py
+++ linkedin_bot.py
@@ -2,6 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+import random
 
 browser = webdriver.Chrome('chromedriver.exe') # Caminho para o Chromedriver
 
